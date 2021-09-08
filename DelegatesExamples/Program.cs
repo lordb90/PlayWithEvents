961548c305714461a2a7eb9976995d88753cda61
BJ Pate <bj.pate@partner.bmwgroup.com>
2021-09-08 10:21:51 -0400

    Initial commit

---
+++ DelegatesExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegatesExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
