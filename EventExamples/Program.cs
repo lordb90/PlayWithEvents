261cdbd6f4ade02f1a39c06a0ced2e56bcfcfdd6
BJ Pate <bj.pate@partner.bmwgroup.com>
2021-09-09 09:09:44 -0400

    update with lamdas and events

---
+++ EventExamples/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EventExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            worker.WorkPerformed += WorkPerformed;
+            worker.WorkCompleted += WorkIsComplete;
+            worker.DoWork(4,WorkType.Reports);
+        }
+
+        private static void WorkIsComplete(object? sender, WorkPerformedEventArgs we)
+        {
+            Console.WriteLine($"Worked that was completed: {we.WorkType} it took {we.Hours} to complete.");
+        }
+
+        private static void WorkPerformed(object? sender, WorkPerformedEventArgs we)
+        {
+            Console.WriteLine($"Worked that was performed: {we.WorkType} for {we.Hours} hour(s)");
+        }
+    }
+}
---
+++ LambdaExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LambdaExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
